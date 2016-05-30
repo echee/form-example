7d4e8bb1c359fa19dd0963cd6498701062a2bd04
Emma Chee <e.chee@hotmail.com>
2016-05-30 18:10:30 +1000

    cleaning up the js

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,35 @@
 $(document).ready(function() {
-  $( "#create-account" ).submit(function( e ) {
-    e.preventDefault();
+    createAccount.init();
+});
 
-    var $form = $( this ),
+var createAccount = {
+  options: {
+    form: "#create-account"
+  },
+  init: function(){
+    this.bindSubmitEvent();
+  },
+  bindSubmitEvent: function(){
+    form = this.options.form;
+    $(form).on('submit', function( e ) {
+      e.preventDefault();
+      createAccount.getSuccessPage(this);
+    });
+  },
+  getSuccessPage: function(form){
+    var $form = $( form ),
       firstName = $form.find( "input[name='first-name']" ).val(),
       url = $form.attr( "action" );
-
     $.ajax({
       method: "POST",
       url: url,
-      data: {name:firstName}
-    }).done(function(data){
-      var content = $(data).find("#success-container");
-      content.find(".page-heading").append(", " + firstName);
-      $( "#form-container" ).empty().append( content );
+    }).done(function(response){
+      createAccount.showSuccessPage(response, firstName);
     });
-  });
-});
+  },
+  showSuccessPage: function(response, firstName){
+    var content = $(response).find("#success-container");
+    content.find(".page-heading").append(", " + firstName + "!");
+    $( "#form-container" ).empty().append( content );
+  }
+};
